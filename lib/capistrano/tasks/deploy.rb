ef7f42657e0fa52ceb2ad4ecc08ba857eda8bd19
Swanand Pagnis <swanand.pagnis@gmail.com>
2019-02-26 17:55:59 +0530

    Use correct namespace

--- lib/capistrano/tasks/deploy.rb
+++ lib/capistrano/tasks/deploy.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 namespace :deploy do
-  desc "Check directories of files to be linked exist in shared"
-  task :make_linked_dirs do
-    next unless any? :linked_files
-
-    on release_roles :all do |_host|
-      execute :mkdir, "-p", deploy_path.join(fetch(:releases_directory, "pinned_releases"))
+  namespace :check do
+    desc "Check shared and release directories exist"
+    task :directories do
+      on release_roles :all do
+        execute :mkdir, "-p", deploy_path.join(fetch(:releases_directory, "pinned_releases"))
+      end
     end
   end
 end
