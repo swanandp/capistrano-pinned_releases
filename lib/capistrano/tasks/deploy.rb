032eecfe484ae1af0e1b975c46d8572546b01b59
Swanand Pagnis <swanand.pagnis@gmail.com>
2019-02-27 14:19:22 +0530

    Add and override original task. Changes will be applied on top of this for easy future "diffing"

--- lib/capistrano/tasks/deploy.rb
+++ lib/capistrano/tasks/deploy.rb
@@ -31,4 +31,36 @@ namespace :deploy do
       end
     end
   end
+
+  desc "Clean up old releases"
+  Rake::Task["deploy:cleanup"].clear_actions
+  task :cleanup do
+    on release_roles :all do |host|
+      releases = capture(:ls, "-x", releases_path).split
+      valid, invalid = releases.partition { |e| /^\d{14}$/ =~ e }
+
+      warn t(:skip_cleanup, host: host.to_s) if invalid.any?
+
+      if valid.count >= fetch(:keep_releases)
+        info t(:keeping_releases, host: host.to_s, keep_releases: fetch(:keep_releases), releases: valid.count)
+        directories = (valid - valid.last(fetch(:keep_releases))).map do |release|
+          releases_path.join(release).to_s
+        end
+        if test("[ -d #{current_path} ]")
+          current_release = capture(:readlink, current_path).to_s
+          if directories.include?(current_release)
+            warn t(:wont_delete_current_release, host: host.to_s)
+            directories.delete(current_release)
+          end
+        else
+          debug t(:no_current_release, host: host.to_s)
+        end
+        if directories.any?
+          execute :rm, "-rf", *directories
+        else
+          info t(:no_old_releases, host: host.to_s, keep_releases: fetch(:keep_releases))
+        end
+      end
+    end
+  end
 end
