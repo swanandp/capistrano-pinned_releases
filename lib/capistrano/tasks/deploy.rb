ad1f2c0a22df1872f09d2de87c6189661bc608de
Swanand Pagnis <swanand.pagnis@gmail.com>
2019-02-27 13:19:11 +0530

    - Add pin:current and pin:list tasks - Stop ignoring rubocop file, and add it to project so that Hound can be enabled.

--- lib/capistrano/tasks/deploy.rb
+++ lib/capistrano/tasks/deploy.rb
@@ -9,4 +9,26 @@ namespace :deploy do
       end
     end
   end
+
+  namespace :pin do
+    desc "Create a pinned release: Symlink current release to pinned_releases"
+    task current: "deploy:check" do
+      on release_roles :all do
+        real_release_path = capture :readlink, release_path.parent.join(current_path.basename)
+        release_name = capture :basename, real_release_path
+        pinned_releases_directory = deploy_path.join(fetch(:releases_directory, "pinned_releases"))
+
+        execute :ln, "-s", real_release_path, pinned_releases_directory.join(release_name)
+      end
+    end
+
+    desc "List all currently pinned releases"
+    task list: "deploy:check" do
+      on release_roles :all do
+        pinned_releases_directory = deploy_path.join(fetch(:releases_directory, "pinned_releases"))
+        pinned_releases = capture :ls, pinned_releases_directory
+        puts pinned_releases.split(/\s+/).sort.join(", ")
+      end
+    end
+  end
 end
